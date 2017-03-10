942c6cd544be5d348c1d140e5b4842ae917eedb4
LisJaz <pitujaz19@hotmail.com>
2017-03-10 18:17:47 -0500

    Segundo comit donde hicimos más cositas, con hola mundo en brackets

---
+++ Ejercicio1/js/script.js
@@ -0,0 +1,8 @@
+//var x=2+3
+//alert("hola mundo "+x);
+
+function meLlamaron(){
+    //document.getElementById('idprueba').innerHTML ="wtf"
+    document.getElementById('idprueba').innerHTML ="<p>hola<p>"
+
+}
